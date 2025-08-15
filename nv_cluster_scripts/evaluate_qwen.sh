18c03790c2a27bf10b67432e95a211c69d390130
zl3466 <zl3466@nyu.edu>
2025-08-15 14:26:59 -0400

    updating scripts for nv_cluster

--- nv_cluster_scripts/evaluate_qwen.sh
+++ nv_cluster_scripts/evaluate_qwen.sh
@@ -79,12 +79,12 @@ for model in "${models[@]}"; do
     "qwen25_72b")
         model_family="qwen25vl"
         model_args="pretrained=Qwen/Qwen2.5-VL-72B-Instruct,download_dir=/lustre/fsw/portfolios/nvr/users/ymingli/projects/zl3466/models/qwen,video_decode_backend=decord,conv_template=qwen_2_5,max_frames_num=64,device_map=auto,modality=video"
-#        num_processes=$num_processes
+        num_processes=1
         ;;
     "qwen25_7b")
         model_family="qwen25vl"
         model_args="pretrained=Qwen/Qwen2.5-VL-7B-Instruct,download_dir=/lustre/fsw/portfolios/nvr/users/ymingli/projects/zl3466/models/qwen,video_decode_backend=decord,conv_template=qwen_2_5,max_frames_num=64,device_map=auto,modality=video"
-        # Allow multi-GPU for 7B model
+        num_processes=1  # Use single process, let device_map handle multi-GPU
         ;;
     *)
         echo "Unknown model: $model"
