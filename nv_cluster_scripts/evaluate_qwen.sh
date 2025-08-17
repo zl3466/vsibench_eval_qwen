0fba0f1a85c86ad6d8d79dd7ff92c9bd39c1bce2
zl3466 <zl3466@nyu.edu>
2025-08-17 01:11:41 -0400

    updating scripts for nv_cluster

--- nv_cluster_scripts/evaluate_qwen.sh
+++ nv_cluster_scripts/evaluate_qwen.sh
@@ -35,7 +35,7 @@ benchmark=vsibench
 output_path=logs/$(TZ="America/New_York" date "+%Y%m%d")
 num_processes=8
 num_frames=32
-launcher=accelerate
+launcher=python
 
 available_models="llava_one_vision_qwen2_0p5b_ov_32f,llava_one_vision_qwen2_7b_ov_32f,llava_next_video_7b_qwen2_32f,llama3_vila1p5_8b_32f,llama3_longvila_8b_128frames_32f,longva_7b_32f,internvl2_2b_8f,internvl2_8b_8f"
 
