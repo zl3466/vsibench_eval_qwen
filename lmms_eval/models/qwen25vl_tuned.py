bf3a7cc5810b1a4f591dece6d0ec77cba74d206b
zl3466 <zl3466@nyu.edu>
2025-09-13 22:14:45 -0400

    increasing gpu_memory_utilization

--- lmms_eval/models/qwen25vl_tuned.py
+++ lmms_eval/models/qwen25vl_tuned.py
@@ -193,7 +193,7 @@ class Qwen25VL_tuned(lmms):
 
             for i, output in enumerate(generated):
                 output_text = output.outputs[0].text
-                print(texts_batch[i])
+                # print(texts_batch[i])
                 # print(f"output text:")
                 # print(output_text)
                 if int(os.getenv("VSI_THOUGHT_PROCESS", "0")) == 1:
